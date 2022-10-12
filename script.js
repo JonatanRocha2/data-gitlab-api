15527329266b19a5ea60784a1dc6c27a64a97980
Jonatan Rocha <jonatanivanhoe@gmai.com>
2022-10-12 20:27:04 -0300

    ajuste do script para paginacao do gitlab

--- script.js
+++ script.js
@@ -4,20 +4,45 @@ const MyHeaders = {
     Authorization: 'Bearer glprd-QXh4AuA-1zK9aMM5jtnf'
 }
 
-fetch('http://gitlab.sefa.pa.gov.br/api/v4/groups/7/projects',
-    { headers: MyHeaders, per_page: 100, page: 4})
+function getProjectId() {
+    //page 1
+    fetch('http://gitlab.sefa.pa.gov.br/api/v4/groups/7/projects?per_page=100&page=1',
+        { headers: MyHeaders})
+            .then(response => {
+                response.json()
+            .then((result) => {
+                result.map((item) => console.log(item.id + ' ' + item.name))
+            })
+    })
+    //page 2
+    fetch('http://gitlab.sefa.pa.gov.br/api/v4/groups/7/projects?per_page=100&page=2',
+        { headers: MyHeaders})
+            .then(response => {
+                response.json()
+            .then((result) => {
+                result.map((item) => console.log(item.id + ' ' + item.name))
+            })
+    })
+    //page 3
+    fetch('http://gitlab.sefa.pa.gov.br/api/v4/groups/7/projects?per_page=100&page=3',
+    { headers: MyHeaders})
         .then(response => {
             response.json()
         .then((result) => {
-            result.map((item) => console.log('Nome --- ' + item.name + 'ID --- ' + item.id))
+            result.map((item) => console.log(item.id + ' ' + item.name))
         })
-})
+    })
+    //page 4
+    fetch('http://gitlab.sefa.pa.gov.br/api/v4/groups/7/projects?per_page=100&page=4',
+    { headers: MyHeaders})
+        .then(response => {
+            response.json()
+        .then((result) => {
+            result.map((item) => console.log(item.id + ' ' + item.name))
+        })
+    })
+}
 
-// fetch('http://gitlab.sefa.pa.gov.br/api/v4/projects/77/integrations',
-//     { method: 'GET', headers: MyHeaders})
-//         .then(response => {
-//             response.json()
-//         .then((result) => {
-//             result.map((item) => console.log('ID --- ' + item.title ))
-//         })
-// })
+getProjectId()
+
+
